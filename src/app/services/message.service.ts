6f4a99be1211a3c8b4697bd714f6bc67261b18e8
Jeremie Bourque <jeremieb@gmail.com>
2019-04-06 13:42:30 -0300

    Messaging component to send messages from any component and receive them in common component

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+/*
+* Receive messages: 
+        this.subscription = this.messageService.getMessage().subscribe(message => {
+          if (message) {
+            this.messages.push(message);
+          } else {
+            // clear messages when empty message received
+            this.messages = [];
+          }
+        });
+*
+
+  Send messages:
+   this.messageService.sendMessage('Message from Home Component to App Component!');
+*/
+
+@Injectable({ providedIn: 'root' })
+export class MessageService {
+    private subject = new Subject<any>();
+
+    sendMessage(message: string) {
+        this.subject.next({ text: message });
+    }
+
+    clearMessages() {
+        this.subject.next();
+    }
+
+    getMessage(): Observable<any> {
+        return this.subject.asObservable();
+    }
+}
