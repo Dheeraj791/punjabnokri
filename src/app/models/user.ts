e250c3c4a5928c10506f9509cb05cedd7d007866
Jeremie Bourque <jeremieb@gmail.com>
2019-04-06 13:58:20 -0300

    Start of employer registration process

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -6,16 +6,18 @@ export class User {
     email: string = '';
     password: string = '';
     address: string = '';
-    userType: string; 
+    userType: string;
 
     constructor(data?: any) {
-        this.username = data.username; 
-        this.firstname = data.firstname; 
-        this.lastname = data.lastname; 
-        this.email = data.email; 
-        this.password = data.password; 
-        this.address = data.address; 
-        this.address = data.address; 
-        this.userType = data.userType; 
+        if (data) {
+            this.username = data.username;
+            this.firstname = data.firstname;
+            this.lastname = data.lastname;
+            this.email = data.email;
+            this.password = data.password;
+            this.address = data.address;
+            this.address = data.address;
+            this.userType = data.userType;
+        }
     }
 }
