8805c3033f989a99b11597572063b1269328adcb
Jeremie Bourque <jeremieb@gmail.com>
2019-04-25 10:45:19 -0300

    Changes to steps in employer registraiton

--- src/app/models/user_profile.ts
+++ src/app/models/user_profile.ts
@@ -14,11 +14,11 @@ export class UserProfile {
     constructor(data?: any) {
         if (data) {
             this.fullTime = data.fullTime;
-            this.partTime = data.partTime;;
-            this.morning = data.morning;;
+            this.partTime = data.partTime;
+            this.morning = data.morning;
             this.afternoon = data.afternoon;
-            this.evening = data.evening;;
-            this.weekend = data.weekend;;
+            this.evening = data.evening;
+            this.weekend = data.weekend;
             this.desiredSalary = data.desiredSalary;
             this.activelySeekingWork = data.activelySeekingWork;
             this.why = data.why;
