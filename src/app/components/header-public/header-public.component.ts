052ea9f52d921ae815c32d6528c89e9cbf85d792
Jeremie Bourque <jeremieb@gmail.com>
2019-07-13 00:39:19 -0300

    Added public header

---
+++ src/app/components/header-public/header-public.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'header-public-component',
+  templateUrl: './header-public.component.html',
+  styleUrls: ['./header-public.component.scss']
+})
+
+export class HeaderPublicComponent implements OnInit {
+
+  selectedType: string;
+  selectedTypes: any = {
+    'employer': 'Job Seeker',
+    'jobseeker': 'Employers'
+  };
+  loggedIn: boolean;
+  @Input() showHome: boolean = false;
+  @Input() showLogin: boolean = false;
+  @Output() userTypeChange: EventEmitter<string> = new EventEmitter();
+
+  constructor(
+    private storage: Storage,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.selectedType = 'jobseeker';
+  }
+
+  ngOnInit() {
+    this.authService.isAuthenticated().then(isLoggedIn => {
+      this.loggedIn = isLoggedIn;
+    });
+
+  }
+
+
+  onSwitchUserType() {
+
+    this.storage.get('user_type').then(res => {
+      const user_type = (res === 'jobseeker' ? 'employer' : 'jobseeker');
+      this.selectedType = user_type;
+      this.storage.set('user_type', user_type);
+      this.userTypeChange.emit(user_type);
+    }
+    );
+  }
+
+  goHome(){
+    this.router.navigate(['/tutorial']);
+  }
+
+
+}
